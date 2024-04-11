639b46416a06b4804dbc9c6586cc86fa28706381
Kimmuie <139369268+Kimmuie@users.noreply.github.com>
2024-04-11 10:55:26 +0700

    Add files via upload

---
+++ extractorTemp.py
@@ -0,0 +1,14 @@
+import json
+
+with open('out_data.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
+
+filtered_templates = []
+for entry in data:
+    if all(not isinstance(app, int) for app in entry["apps"]):
+        filtered_templates.append(entry)
+
+with open('TemplateBad2.json', 'w', encoding='utf-8') as outfile:
+    json.dump({'templates': filtered_templates}, outfile, indent=4)
+
+print("Extraction completed. Data written to TemplateGood.json.")
